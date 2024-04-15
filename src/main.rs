4dd5c315719ca9ea4f50111ed2a87eda4aa5d1f6
Leo Giband <leo.giband@gmail.com>
2024-04-15 15:12:49 +0200

    Setup unit test

--- src/main.rs
+++ src/main.rs
@@ -2,72 +2,73 @@ mod matrix;
 mod vector;
 mod test;
 
-use matrix::Matrix;
-use vector::Vector;
 
-
+#[cfg(test)]
+#[test]
 fn vector_ex00() {
+    use vector::Vector;
+    
+    println!("===============Vector Ex00===============");
     let mut u = Vector::from(vec![2., 3.]);
     let v = Vector::from(vec![5., 7.]);
     u.add(&v);
-    println!("{}", u);
-    // [7.0]
-    // [10.0]
+    assert_eq!(u.vector, vec![7., 10.]);
+
     let mut u = Vector::from(vec![2., 3.]);
     let v = Vector::from(vec![5., 7.]);
     u.sub(&v);
-    println!("{}", u);
-    // [-3.0]
-    // [-4.0]
+    assert_eq!(u.vector, vec![-3., -4.]);
+
     let mut u = Vector::from(vec![2., 3.]);
     u.scl(2.);
-    println!("{}", u);
-    // [4.0]
-    // [6.0]
+    assert_eq!(u.vector, vec![4., 6.]);
 }
 
+#[cfg(test)]
+#[test]
 fn matrix_ex00() {
+    use matrix::Matrix;
+
+    println!("===============Matrix Ex00===============");
     let mut u = Matrix::from(vec![
-        vec![1., 2.],
-        vec![3., 4.]
+            vec![1., 2.],
+            vec![3., 4.]
         ]);
         let v = Matrix::from(vec![
-        vec![7., 4.],
-        vec![-2., 2.]
+            vec![7., 4.],
+            vec![-2., 2.]
         ]);
         u.add(&v);
-        println!("{}", u);
-        // [8.0, 6.0]
-        // [1.0, 6.0]
+        assert_eq!(u.matrix, vec![
+            vec![8., 6.],
+            vec![1., 6.]
+        ]);
         let mut u = Matrix::from(vec![
-        vec![1., 2.],
-        vec![3., 4.]
+            vec![1., 2.],
+            vec![3., 4.]
         ]);
         let v = Matrix::from(vec![
-        vec![7., 4.],
-        vec![-2., 2.]
+            vec![7., 4.],
+            vec![-2., 2.]
         ]);
         u.sub(&v);
-        println!("{}", u);
-        // [-6.0, -2.0]
-        // [5.0, 2.0]
+        assert_eq!(u.matrix, vec![
+            vec![-6., -2.],
+            vec![5., 2.]
+        ]);
         let mut u = Matrix::from(vec![
-        vec![1., 2.],
-        vec![3., 4.]
+            vec![1., 2.],
+            vec![3., 4.]
         ]);
         u.scl(2.);
-        println!("{}", u);
+        assert_eq!(u.matrix, vec![
+            vec![2., 4.],
+            vec![6., 8.]
+        ]);
         // [2.0, 4.0]
         // [6.0, 8.0]
 }
 
 fn main() {
-    vector_ex00();
-    matrix_ex00();
-    //println!("{}", matrix);
-    //println!("matrix1: shape ={:?}, is_square={:?}", matrix.shape(), matrix.is_square());
-
-    //let vector = Vector::from(vec!([25.0, 5.5884, 12.0, 98412.0]));
-    //println!("{}", vector);
-    //println!("{}", vector.size());
+    println!("Please run `cargo test`");
 }
