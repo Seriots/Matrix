8bc374083172e09125102ebd29f7f69af7b1ef81
Leo Giband <leo.giband@gmail.com>
2024-04-23 17:39:54 +0200

    Projection in main

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,8 @@ use crate::core::*;
 fn main() {
     println!("Please run `cargo test`");
 
-    let u = Vector::from(&[Complex::new(1., 2.), Complex::new(3., 4.)]);
-
-    let a = Complex::new(1., 2.);
-    let b = Complex::new(3., 4.);
-    println!("u = {}", a * b);
+    let proj_mat = projection(90., 1., 0.01, 20.);
+    save_matrix(&proj_mat, "proj");
 }
 
 
